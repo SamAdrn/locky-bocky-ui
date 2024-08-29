ceedfd91fdf9644ce26dd8b31a27b73d0af23383
Samuel Kosasih <samadrianko@gmail.com>
2024-08-28 21:28:24 -0400

    Built Site List Element to display a single Site

--- src/app/sitelist/site-list.module.ts
+++ src/app/sitelist/site-list.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
 import { CheckboxModule } from 'primeng/checkbox'
-import { PanelModule } from 'primeng/panel'
+import { SpeedDialModule } from 'primeng/speeddial'
 
 import { SiteListComponent } from './site-list.component'
 
 @NgModule({
     declarations: [SiteListComponent],
-    imports: [CommonModule, FormsModule, CheckboxModule, PanelModule],
+    imports: [CommonModule, FormsModule, CheckboxModule, SpeedDialModule],
     exports: [SiteListComponent],
 })
 export class SiteListModule {}
