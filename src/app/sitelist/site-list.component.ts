6dd029dde90b4cd9fa84495e313caff1200a7557
Samuel Kosasih <samadrianko@gmail.com>
2024-08-28 22:34:35 -0400

    Incorporated the DataView component

--- src/app/sitelist/site-list.component.ts
+++ src/app/sitelist/site-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
+import { SITE_MOCK_DATA } from 'src/shared/mock-data/site-mock.data'
 
 @Component({
     selector: 'app-sitelist',
     templateUrl: './site-list.component.html',
 })
 export class SiteListComponent {
-    checked: boolean = false
+    checked: { [id: string]: boolean } = {}
+    sites = SITE_MOCK_DATA
 }
--- src/app/sitelist/site-list.module.ts
+++ src/app/sitelist/site-list.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
+import { ButtonModule } from 'primeng/button'
 import { CheckboxModule } from 'primeng/checkbox'
+import { DataViewModule } from 'primeng/dataview'
 import { SpeedDialModule } from 'primeng/speeddial'
 
 import { SiteListComponent } from './site-list.component'
 
 @NgModule({
     declarations: [SiteListComponent],
-    imports: [CommonModule, FormsModule, CheckboxModule, SpeedDialModule],
+    imports: [
+        CommonModule,
+        ButtonModule,
+        DataViewModule,
+        FormsModule,
+        CheckboxModule,
+        SpeedDialModule,
+    ],
     exports: [SiteListComponent],
 })
 export class SiteListModule {}
